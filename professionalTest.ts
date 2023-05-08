a7fdfda9c4ce28997c9c1bf22097bdf87676f899
Feer95 <fernandofs.ffs@gmail.com>
2023-05-08 21:13:46 +0200

    primerCommit

---
+++ professionalTest.ts
@@ -0,0 +1,11 @@
+import { Professional } from './professional';
+
+let prof1 = new Professional ("Mario", 30, 80, 180, false, "Española", 0, "Camarero")
+let prof2 = new Professional ("Myke", 42, 90, 190, false, "Americano", 0, "Empresario")
+let prof3 = new Professional ("Puri", 60, 70, 167, true, "Española", 5, "Actriz")
+let prof4 = new Professional ("Dimitri", 20, 100, 200, false, "Rumana", 0, "Boxeador")
+
+prof1.printAll();
+prof2.printAll();
+prof3.printAll();
+prof4.printAll();
